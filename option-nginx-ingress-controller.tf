87fa570b84a6f3b069e5af6682db63d579a70127
Jo Brunner <jo.brunner@mayflower.de>
2024-02-08 11:40:05 +0100

    Fixes race condition with namespaces

--- option-nginx-ingress-controller.tf
+++ option-nginx-ingress-controller.tf
@@ -4,7 +4,8 @@ data "kubectl_file_documents" "nginx_ingress_manifests" {
 }
 
 resource "kubectl_manifest" "kubectl_apply_ingress" {
-  count      = var.enable_ingress && var.ingress_controller == "Nginx" ? length(data.kubectl_file_documents.nginx_ingress_manifests[0].documents) : 0
-  yaml_body  = element(data.kubectl_file_documents.nginx_ingress_manifests[0].documents, count.index)
-  depends_on = [kind_cluster.this]
+  count             = var.enable_ingress && var.ingress_controller == "Nginx" ? length(data.kubectl_file_documents.nginx_ingress_manifests[0].documents) : 0
+  yaml_body         = element(data.kubectl_file_documents.nginx_ingress_manifests[0].documents, count.index)
+  server_side_apply = true
+  depends_on        = [kind_cluster.this]
 }
